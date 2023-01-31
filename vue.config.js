44cc86cdf2ebb63eedfd5f8b35a4f25c9319b2c6
SheenX <edilyxin@sina.com>
2023-01-31 21:57:17 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,   //关闭eslint检查
+  transpileDependencies: true,
+  devServer: {         // 解决默认打开 0.0.0.0的问题
+    open: true,
+    host: 'localhost',
+    port: 8080
+  }
 })
